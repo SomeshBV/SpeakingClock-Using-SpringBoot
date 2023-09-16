5bc3ca3be94d3c461259022ecb18802a061af1a4
unknown <somesh4287@gmail.com>
2023-09-16 09:22:16 +0530

    First Commit

--- speakingclock/src/main/java/com/speakingclock/speakingclock/SpeakingclockApplication.java
+++ speakingclock/src/main/java/com/speakingclock/speakingclock/SpeakingclockApplication.java
@@ -3,7 +3,15 @@ package com.speakingclock.speakingclock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "SpeakingClock",
+				description = "This is a Java Spring Boot application upon providing time in number format will showup time in words. This application is written in microservice architecture with api implementation.",
+				version = "1.0.0"))
 public class SpeakingclockApplication {
 
 	public static void main(String[] args) {
--- speakingclock/src/main/java/com/speakingclock/speakingclock/config/SwaggerConfig.java
+++
@@ -1,5 +0,0 @@
-package com.speakingclock.speakingclock.config;
-
-public class SwaggerConfig {
-
-}
